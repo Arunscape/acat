cbe17195939faf52cc50a208b61fbe01bbc719c4
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-11-15 16:33:36 -0700

    did it without the ansi library

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use std::io;
 use std::io::Read;
+use rand::Rng;
 
-fn main() -> io::Result<()> {
+fn paint(r:u8, g:u8, b:u8) {
+    let esc = char::from(0x1b);
+    print!("{}[38;2;{};{};{}m", esc, r, g, b);
+}
 
-    let mut buffer = Vec::new();
-    io::stdin().read_to_end(&mut buffer)?;
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
 
-    for line in buffer {
-        println!("{}", line);
+    let mut rng = rand::thread_rng();
+    for c in buffer.chars() {
+
+        paint(rng.gen(), rng.gen(), rng.gen());
+        print!("{}" ,c);        
     }
-    println!("Hello, world!");
 
-    Ok(())
+    println!();
+
+
 }
