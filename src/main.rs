2fe3797b29338db0e9c1e8f7765c83e2fb7695f3
Arunscape <8227297+Arunscape@users.noreply.github.com>
2020-02-13 12:12:14 -0700

    save this

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::io;
 use rand::Rng;
+use std::io;
 use std::io::prelude::*;
 
-fn paint(r:u8, g:u8, b:u8) {
+fn paint(r: u8, g: u8, b: u8) {
     let esc = char::from(0x1b);
     print!("{}[38;2;{};{};{}m", esc, r, g, b);
 }
@@ -10,17 +10,13 @@ fn paint(r:u8, g:u8, b:u8) {
 fn main() {
     let mut rng = rand::thread_rng();
 
-        for line in io::stdin().lock().lines() {
-
-            for c in line.unwrap().chars() {
-
+    for line in io::stdin().lock().lines() {
+        for c in line.unwrap().chars() {
             paint(rng.gen(), rng.gen(), rng.gen());
-            print!("{}" ,c);        
+            print!("{}", c);
         }
-            println!();
+        println!();
     }
 
     println!();
-
-
 }
