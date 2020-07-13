dd9d5a8b3cc224d99ffb29e2eda3cd658ea46016
Arunscape <arun@woosaree.xyz>
2020-07-12 23:57:09 -0600

    getopts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![deny(clippy::all)]
+
+use getopts::Options;
 use rand::Rng;
 use std::fs::File;
 use std::usize::MAX;
@@ -45,6 +48,28 @@ fn read_file_plain(file: &String) -> () {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    let mut opts = Options::new();
+    opts.optflag("A", "show-all", "equivalent to -vET");
+    opts.optflag(
+        "b",
+        "number-nonblank",
+        "number nonempty output lines, overrides -n",
+    );
+    opts.optflag("e", "", "equivalent to -vE");
+    opts.optflag("E", "show-ends", "display $ at end of each line");
+    opts.optflag("n", "number", "number all output lines");
+    opts.optflag("s", "squeeze-blank", "suppress repeated empty output lines");
+    opts.optflag("t", "", "equivalent to -vT");
+    opts.optflag("T", "show-tabs", "display TAB characters as ^I");
+    opts.optflag("u", "", "(ignored)");
+    opts.optflag(
+        "v",
+        "show-nonprinting",
+        "use ^ and M- notation, except for LFD and TAB",
+    );
+    opts.optflag("", "version", "output version information and exit");
+
     match args.len() {
         1 => read_stdin(),
         (2..=MAX) => read_file(&args),
